99d584835a04be875fb7e92375a2e6d89ce71e60
Nguyen Tien Dung <dungnguyen5@cqtdt.asia>
2024-12-10 10:10:45 +0700

    test: show ci/cd failure results

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+
 function Test() {
-  return <div>Test success</div>;
+  return (
+    <div>
+      Test success
+      <Image src="/test.svg" alt="test" height={10} width={10} />
+    </div>
+  );
 }
 
 export default Test;
