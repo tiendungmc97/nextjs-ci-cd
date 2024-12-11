7b69cb7389a856a0bcea92aa594284b775786cb3
Nguyen Tien Dung <dungnguyen5@cqtdt.asia>
2024-12-11 10:07:48 +0700

    config: setup mutil language for project

--- scripts/lint-fix.js
+++ scripts/lint-fix.js
@@ -1,14 +1,12 @@
 const { exec } = require("child_process");
 
-exec(
-  "eslint src --ext js,jsx,ts,tsx --fix --cache",
-  (error, stdout, stderr) => {
-    if (error) {
-      console.error(stderr);
-      process.exit(1);
-    } else {
-      console.log(stdout);
-      console.log("Lint fix applied successfully!");
-    }
+exec("eslint src --ext js,jsx,ts,tsx --fix --cache", (error, stdout, stderr) => {
+  if (error) {
+    console.error(stderr);
+    console.error(stdout);
+    process.exit(1);
+  } else {
+    console.log(stdout);
+    console.log("Lint fix applied successfully!");
   }
-);
+});
--- src/app/Home/page.tsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <div>Home Test test</div>;
-}
-
-export default Home;
